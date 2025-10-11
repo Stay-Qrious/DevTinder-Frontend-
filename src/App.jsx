0f55fb4db46f5595d5561a9eacb97fba94fba781
Abhishek Tripathi <abhishektripathi8582@gmail.com>
2025-10-11 16:47:11 +0530

    Hello World

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>
+        this is start of prject
+      </h1>
+
+    </>
+  )
+}
+
+export default App
