5c9b2859cab9d0518aef1f834cae2958f49159a1
Abhishek Tripathi <abhishektripathi8582@gmail.com>
2025-10-12 14:27:22 +0530

    recent commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,61 @@
-
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleSubmit = async () => {
+    try {
+      console.log("Submitting...");
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        { email, password },
+        { withCredentials: true }
+      );
+      console.log("Response:", res.data);
+    } catch (err) {
+      console.error("Login failed:", err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title flex justify-center">Login</h2>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend text-lg">Email</legend>
+            <input
+              type="text"
+              className="input"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend text-lg">Password</legend>
+            <input
+              type="password"
+              className="input"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </fieldset>
+
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary" onClick={handleSubmit}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
